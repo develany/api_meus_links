0af15bd4d06aeb6beaf026386c1e252be53cf1f9
John Lennon <elanysantos5@outlook.com>
2024-01-08 22:21:10 -0300

    bug fix

--- src/app.js
+++ src/app.js
@@ -6,5 +6,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(rotas)
-
-app.listen(process.env.PORT);
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});;
